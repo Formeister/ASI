e79332b501be99202d68ea15666a96ff16a6c251
Formeister <mefis.bartek@gmail.com>
2017-03-20 18:29:46 +0100

    Logowanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   resources :days
-
   resources :users
 
   root 'home#index'
 
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 end
